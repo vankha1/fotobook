8250ad71c7629fa91ae87596e5197c796d369138
vankha1 <vovankha2003@gmail.com>
2024-06-27 20:38:37 +0700

    feat: add validation and callbacks

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,7 @@
 class Follow < ApplicationRecord
     belongs_to :follower, class_name: "User"
     belongs_to :followed, class_name: "User"
+    
+    validates :user_id, presence: true
+    validates :follower_id, presence: true
 end
