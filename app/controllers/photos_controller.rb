4879ba9ed49a8fc16adcdaf671d367f7b42e6585
vankha1 <vovankha2003@gmail.com>
2024-07-05 16:21:05 +0700

    feat: index action photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,14 +1,10 @@
 class PhotosController < ApplicationController
     def index
-        @photos = [
-            {
-                image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzC2JyZDZ_RaWf0qp11K0lcvB6b6kYNMoqtZAQ9hiPZ4cTIOB",
-                title: "vankha",
-                description: "check here",
-                number_like: 1,
-                created_at: ""
-            }
-        ]
+        if user_signed_in?
+            @photos = current_user.photos
+        else
+            @photos = Photo.all
+        end
     end
 
     def new 
