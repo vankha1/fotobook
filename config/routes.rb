8793f297eb3d9d501cf3084c1299c1f42d75c6aa
vankha1 <vovankha2003@gmail.com>
2024-07-05 08:38:13 +0700

    feat: add devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "photos#index"
+  devise_for :users
   get '/login', to: "auths#login"
   get '/signup', to: "auths#signup"
 
--- db/migrate/20240626070455_create_users.rb
+++ db/migrate/20240626070455_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
       t.string :first_name
       t.string :last_name
       t.string :email
-      t.string :password
+      t.boolean :is_active
       t.boolean :is_admin
       t.string :avatar
 
