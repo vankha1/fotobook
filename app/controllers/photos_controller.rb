50abae6c9095152f9ff1b58f950424ed6d49cdfe
vankha1 <vovankha2003@gmail.com>
2024-07-05 09:46:45 +0700

    fix: sign up ui

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,14 @@
 class PhotosController < ApplicationController
     def index
-        @photos = User.find(2).photos
+        @photos = [
+            {
+                image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzC2JyZDZ_RaWf0qp11K0lcvB6b6kYNMoqtZAQ9hiPZ4cTIOB",
+                title: "vankha",
+                description: "check here",
+                number_like: 1,
+                created_at: ""
+            }
+        ]
     end
 
     def new 
