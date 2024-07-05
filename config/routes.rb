340169b34fe044a4924595b3aff0df40cdcc9f57
vankha1 <vovankha2003@gmail.com>
2024-07-05 09:08:20 +0700

    feat: sign up with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations',
+  }
   root "photos#index"
-  devise_for :users
   get '/login', to: "auths#login"
   get '/signup', to: "auths#signup"
 
