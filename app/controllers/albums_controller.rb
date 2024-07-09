88024ef8bb0d4de2b5b354e8d53ece234dbbc20d
vankha1 <vovankha2003@gmail.com>
2024-07-09 14:14:47 +0700

    feat: handle preview photos in album creation

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -4,7 +4,18 @@ class AlbumsController < ApplicationController
     end
 
     def new 
-        
+        @album = Album.new
+    end
+
+    def create
+        @album = current_user.albums.new(album_params)
+        @photos = params[:album][:photos]
+
+        if @album.save!
+            redirect_to @album, notice: 'Album was successfully created.'
+        else
+            render :new
+        end
     end
 
     def edit
@@ -13,5 +24,16 @@ class AlbumsController < ApplicationController
     
     def discover
          
+    end
+
+    private
+
+    def album_params
+        params.require(:album).permit(:title, :description, :is_private, photos_attributes: [:title, :description, :image_url, :is_private])
+    end
+
+
+    def photo_params
+        params.require(:photo).permit(:title, :description, :image_url, :is_private)
     end
 end
