fa9a5f82834e7e626244ac185d7687b6049b4815
vankha1 <vovankha2003@gmail.com>
2024-07-14 14:13:20 +0700

    fix: photos reaction by using ajax jquery

--- app/controllers/photos_reactions_controller.rb
+++ app/controllers/photos_reactions_controller.rb
@@ -1,33 +1,19 @@
 class PhotosReactionsController < ApplicationController
     
-    before_action :find_photo
+    skip_before_action :verify_authenticity_token
 
     def create
         @photo = Photo.find(params[:photo_id])
         current_user.photos_reactions.create!(photo_id: params[:photo_id])
         @photo.number_like += 1
         @photo.save
-        respond_to do |format|
-            format.html { redirect_to root_path }
-            format.js 
-        end
     end
 
     def destroy
+        p params
         @photo = Photo.find(params[:id])
         current_user.photos_reactions.where(photo_id: params[:id]).destroy_all
         @photo.number_like -= 1
         @photo.save
-        respond_to do |format|
-            format.html { redirect_to root_path }
-            format.js 
-        end
-    end
-
-    private
-
-    def find_photo
-        photoId = params[:photo_id] || params[:id]
-        photos = Photo.find(photoId)
     end
 end
